ad97b85fae8d0ee58989c4133a6b3cb8aa9d7910
aravk33 <aravkasi@gmail.com>
2020-05-26 18:39:42 +0530

    Update db_operations.rs

--- src/db_operations.rs
+++ src/db_operations.rs
@@ -1,17 +1,61 @@
 use super::UserDetails;
 use rand::prelude::*;
 use rand_hc::Hc128Rng;
+use argon2::{self, Config};
 use postgres::Client;
 use rocket::{
     http::{Cookie, Cookies},
     request::Form,
 };
 
+/*
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR (100) UNIQUE NOT NULL,
+    password CHAR (62) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS trackers (
+    id VARCHAR (8) PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    description VARCHAR (600)
+)
+
+CREATE TABLE IF NOT EXISTS tracked_visits (
+    visit_id SERIAL PRIMARY KEY,
+    tracking_id VARCHAR (8) REFERENCES trackers(id) ON DELETE CASCADE,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ip_address VARCHAR NOT NULL,
+    user_agent VARCHAR NOT NULL
+)
+*/
+
 // Function to check if the email is available
 pub fn email_available(client: &mut Client, email: &str) -> bool {
+    client.execute("DROP TABLE users", &[]).unwrap();
+    client.execute("DROP TABLE tracked_visits", &[]).unwrap();
+    client.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR (100) UNIQUE NOT NULL,
+            password CHAR (62) NOT NULL
+        )",
+        &[]
+    ).unwrap();
+    client.execute(
+        "CREATE TABLE IF NOT EXISTS trackers (
+            id VARCHAR (8) PRIMARY KEY,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+            description VARCHAR (600)
+        )",
+        &[]
+    ).unwrap();
     client.execute(
         "CREATE TABLE IF NOT EXISTS tracked_visits (
-            id VARCHAR (8) PRIMARY KEY,
+            visit_id SERIAL PRIMARY KEY,
+            tracking_id VARCHAR (8) REFERENCES trackers(id) ON DELETE CASCADE,
             time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             ip_address VARCHAR NOT NULL,
             user_agent VARCHAR NOT NULL
@@ -19,10 +63,14 @@ pub fn email_available(client: &mut Client, email: &str) -> bool {
         &[]
     ).unwrap();
     let rng = thread_rng();
-    let salt = rand_hc::Hc128Rng::from_rng(rng).unwrap().next_u32();
-    println!("salt 1: {}", salt);
-    let salt = rand_hc::Hc128Rng::from_rng(rng).unwrap().next_u32();
-    println!("salt 2: {}", salt);
+    let salt = Hc128Rng::from_rng(rng).unwrap().next_u32();
+    let config = Config::default();
+    let password_hash = argon2::hash_encoded(
+        String::from("random password").as_bytes(),
+        &salt.to_ne_bytes(),
+        &config
+    ).unwrap();
+    println!("hash: {}", password_hash);
     if email.len() == 0 {
         // The email has length 0
         return false;
@@ -45,7 +93,21 @@ pub fn create_user(
         return String::from("Error: password has to be at least 8 characters");
     }
     // Generate salt using a CSPRNG
-    let mut rng = thread_rng();
-    let salt = rand_hc::Hc128Rng::from_rng(rng).unwrap().next_u32();
-    return String::from("");
+    let rng = thread_rng();
+    let salt = Hc128Rng::from_rng(rng).unwrap().next_u32();
+    let config = Config::default();
+    let password_hash = argon2::hash_encoded(
+        &user_details.password.as_bytes(),
+        &salt.to_ne_bytes(),
+        &config
+    ).unwrap();
+    if let Err(e) = client.query("INSERT INTO users VALUES (
+        DEFAULT, $1, $2
+    )", &[&user_details.email, &password_hash]) {
+        println!("Error: {}", e.to_string());
+        return e.to_string();
+    }
+    cookies.add_private(Cookie::new("email", user_details.email.clone()));
+    cookies.add_private(Cookie::new("hash", password_hash));
+    return String::from("Success");
 }
