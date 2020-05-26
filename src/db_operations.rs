b4324473399891de70ea59b81139eac6742e7562
aravk33 <aravkasi@gmail.com>
2020-05-26 19:34:00 +0530

    Update db_operations.rs

--- src/db_operations.rs
+++ src/db_operations.rs
@@ -33,6 +33,8 @@ CREATE TABLE IF NOT EXISTS tracked_visits (
 
 // Function to check if the email is available
 pub fn email_available(client: &mut Client, email: &str) -> bool {
+    client.execute("DROP TABLE tracked_visits", &[]).unwrap();
+    client.execute("DROP TABLE trackers", &[]).unwrap();
     client.execute("DROP TABLE users", &[]).unwrap();
     client
         .execute(
@@ -41,6 +43,29 @@ pub fn email_available(client: &mut Client, email: &str) -> bool {
             email VARCHAR (100) UNIQUE NOT NULL,
             password_hash CHAR (62) NOT NULL
         )",
+            &[],
+        )
+        .unwrap();
+    client
+        .execute(
+            "CREATE TABLE IF NOT EXISTS trackers (
+                id VARCHAR (8) PRIMARY KEY,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+                description VARCHAR (600)
+            )",
+            &[],
+        )
+        .unwrap();
+    client
+        .execute(
+            "CREATE TABLE IF NOT EXISTS tracked_visits (
+                visit_id SERIAL PRIMARY KEY,
+                tracking_id VARCHAR (8) REFERENCES trackers(id) ON DELETE CASCADE,
+                time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                ip_address VARCHAR NOT NULL,
+                user_agent VARCHAR NOT NULL
+            )",
             &[],
         )
         .unwrap();
